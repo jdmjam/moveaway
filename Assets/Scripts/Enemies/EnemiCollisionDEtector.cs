ec51eeb43e29084effb1af0f726a7a3c3f2829e8
Gabriel <gabrielcasares.andrade@gmail.com>
2019-04-13 19:56:04 +0100

    conception of the enemies

---
+++ Assets/Scripts/Enemies/EnemiCollisionDEtector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiCollisionDEtector : MonoBehaviour
+{
+    [SerializeField] private LayerMask enemieLayerMask;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        int objectLayer = collision.gameObject.layer;
+        if ((enemieLayerMask&(1<<objectLayer))>0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
